a0c9fba3358cdf0190cb6fc4505c60391d0721a6
soravitpuengsema <s6301012630184@email.kmutnb.ac.th>
2021-11-08 11:40:47 +0700

    first commit

---
+++ quiz/test.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+public class test {
+	public static void main (String[] args){
+		String path = "/home/soravitp/test0";
+		String[] listpath = path.split("/");
+        	String lastpath = listpath[listpath.length-1];
+        	System.out.println(lastpath);
+		findPath(path,0);	
+	}
+	public static void findPath(String path, int count){
+		String[] listpath = path.split("/");
+		String lastpath = listpath[listpath.length-1];
+		File f = new File(path);
+		File[] files =  f.listFiles();
+		
+		for ( int i = 0 ; i < files.length ; i++ ){
+			if ( count > 0 ) {
+			      for ( int j = -1 ; j < count ; j++){
+				System.out.print("      ");                       
+			      }
+			    }
+			    System.out.print("L");
+			    System.out.print("_____");
+		    File file = files[i];
+		    String filename = file.getName();
+		    if ( file.isDirectory() ){
+		    System.out.println(filename);
+		        String nextpath = files[i].toString();
+		        findPath(nextpath, count+1);
+		    }
+		    else {
+		        System.out.println(filename);
+			}
+		}
+	}
+}
